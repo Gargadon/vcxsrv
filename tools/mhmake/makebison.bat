2849a78d463e35b48feac09300df531e23650279
David Kantun <dkantun@gmail.com>
2026-08-25 18:44:48 -0600

    Install native Flex and Bison tools in CI

--- tools/mhmake/makebison.bat
+++ tools/mhmake/makebison.bat
@@ -4,6 +4,6 @@ setlocal
 set BISON_PKGDATADIR=src/bisondata
 
 win_bison.exe -d -Ssrc/bisondata/skeletons/lalr1.cc -o%1/mhmakeparser.cpp src\mhmakeParser.y
-c:\Python39\python.exe addstdafxh.py %1\mhmakeparser.cpp
+python.exe addstdafxh.py %1\mhmakeparser.cpp
 
 endlocal
