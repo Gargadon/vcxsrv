ae0f5d8a7ca2de7ce3852b5f07db1a5bd0d63b1f
David Kantun <dkantun@gmail.com>
2026-08-25 19:16:30 -0600

    Use Cygwin Bison for mhmake generation

--- tools/mhmake/makebison.bat
+++ tools/mhmake/makebison.bat
@@ -3,7 +3,7 @@ setlocal
 
 set BISON_PKGDATADIR=src/bisondata
 
-win_bison.exe -d -Ssrc/bisondata/skeletons/lalr1.cc -o%1/mhmakeparser.cpp src\mhmakeParser.y
+bison.exe -d -Ssrc/bisondata/skeletons/lalr1.cc -o%1/mhmakeparser.cpp src\mhmakeParser.y
 python.exe addstdafxh.py %1\mhmakeparser.cpp
 
 endlocal
