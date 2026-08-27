3bf374d0c238360c982cffb18534338605a5fddc
David Kantun <dkantun@gmail.com>
2026-08-27 13:42:33 -0600

    Corrección

--- tools/mhmake/makebison.bat
+++ tools/mhmake/makebison.bat
@@ -3,8 +3,8 @@ setlocal
 
 set BISON_PKGDATADIR=src/bisondata
 
-if defined CYGWIN_ROOT (
-  win_bison.exe -d -Ssrc/bisondata/skeletons/lalr1.cc -o%1/mhmakeparser.cpp src\mhmakeparser.y
+if defined WIN_BISON_PATH (
+  "%WIN_BISON_PATH%" -d -Ssrc/bisondata/skeletons/lalr1.cc -o%1/mhmakeparser.cpp src\mhmakeparser.y
 ) else (
   win_bison.exe -d -Ssrc/bisondata/skeletons/lalr1.cc -o%1/mhmakeparser.cpp src\mhmakeparser.y
 )
