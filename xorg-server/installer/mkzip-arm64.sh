4cc3901b164a4354401d5caef0b267241492991f
David Kantun <dkantun@gmail.com>
2026-09-08 22:38:42 -0600

    Make ARM64 packaging tolerate missing xkbdata

--- xorg-server/installer/mkzip-arm64.sh
+++ xorg-server/installer/mkzip-arm64.sh
@@ -16,6 +16,15 @@ add_if_present() {
     echo "Skipping missing optional file: $dir/$file"
   fi
 }
+add_dir_if_present() {
+  local dir="$1"
+  local name="$2"
+  if [ -d "$dir/$name" ]; then
+    add -C "$dir" "$name"
+  else
+    echo "Skipping missing optional directory: $dir/$name"
+  fi
+}
 
 tar -cf "$OUTFILE" -C ../objARM64/servrelease vcxsrv.exe
 add -C ../dix protocol.txt
@@ -33,6 +42,7 @@ add_if_present ../../libXmu/src/objARM64/release libXmu.dll
 add -C ../../openssl/arm64 libcrypto-3-arm64.dll libssl-3-arm64.dll
 add -C ../../freetype/objs/ARM64/Release freetype.dll
 add vcruntime140.dll vcruntime140_1.dll msvcp140.dll
-add -C .. xkbdata locale bitmaps fonts
+add_dir_if_present .. xkbdata
+add -C .. locale bitmaps fonts
 
 gzip -f "$OUTFILE"
